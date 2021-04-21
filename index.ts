8badaffb8dd171a5b0637632b0dc9b0d16b60f78
muhammedaydogan <muhammed.aydogan@aol.com>
2021-04-21 16:41:59 +0300

    search functions turned to paralel async functions.

--- index.ts
+++ index.ts
@@ -1,11 +1,19 @@
-import { Product } from "./product.interface";
-import { IWebsite } from "./adapter/interfaces/website.interface";
 import { Trendyol } from "./adapter/our-classes/trendyol";
+import { GratisAdapter } from "./adapter/adaptees/gratis.adaptee";
+import { WatsonsAdapter } from "./adapter/adaptees/watsons.adaptee";
 
 export async function search(searchKey: string) {
-  let res = { categories: [], brands: [], data: [] as Product[], error: "" };
+  let result = { trendyol: {}, gratis: {}, watsons: {} };
 
   let trendyol = new Trendyol();
-  let result = await trendyol.searchProduct("ruj");
-  console.log(result);
+  let gratis = new GratisAdapter();
+  let watsons = new WatsonsAdapter();
+
+  [result.trendyol, result.gratis, result.watsons] = await Promise.all([
+    trendyol.searchProduct(searchKey),
+    gratis.searchProduct(searchKey),
+    watsons.searchProduct(searchKey),
+  ]);
+
+  return result;
 }
